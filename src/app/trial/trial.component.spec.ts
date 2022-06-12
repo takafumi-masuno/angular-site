e9632f54e58f41637842fdd3e89c03f0817cba87
takafumi-masuno <t.masuno@cocolofun.co.jp>
2022-06-12 11:36:16 +0900

    feature: trialページとcontactページの追加

---
+++ src/app/trial/trial.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TrialComponent } from './trial.component';
+
+describe('TrailComponent', () => {
+  let component: TrialComponent;
+  let fixture: ComponentFixture<TrialComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TrialComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TrialComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/trial/trial.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trial',
+  templateUrl: './trial.component.html',
+  styleUrls: ['./trial.component.sass'],
+})
+export class TrialComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

a8a9d817fc2f1d51aa41df292ca14325a9d4062d
takafumi-masuno <t.masuno@cocolofun.co.jp>
2022-06-12 11:36:16 +0900

    feature: trialページとcontactページの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { TrialComponent } from './trial/trial.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'trial', component: TrialComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
